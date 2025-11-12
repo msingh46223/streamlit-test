855cf05e6d0f0d90051a5a86946b304a861d52bf
msingh46223 <msingh46@buffalo.edu>
2025-11-12 11:06:39 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,21 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("Sample User Interface")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Click the button to import your file."
 )
+
+uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+if uploaded_file:
+    st.write("File uploaded!")
+    st.write("**Select Raymond-Roy classification:**")
+    stage = st.radio("Raymond-Roy class:",[1,2,3])
+#    st.radio("Pick one", ["cats", "dogs"])
+
+    match stage:
+        case 1:
+            st.write("Recurrence probability is low")
+        case 2:
+            st.write("Recurrence probability is moderate")
+        case 3:
+            st.write("Recurrence probability is high")
